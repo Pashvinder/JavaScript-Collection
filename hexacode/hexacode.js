959574faa0e4635e02ab1eef4957e164e1b97655
Pashvinder <dhimanpashvinder@gmail.com>
2026-03-29 13:10:05 +0530

    folder created

---
+++ hexacode/hexacode.js
@@ -0,0 +1,11 @@
+let el=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+
+let bg=document.body;
+let btn=document.getElementById("change")
+btn.onclick=function(){
+    let hex="#";
+    for(let i=0;i<6;i++){
+    hex+=el[Math.floor(Math.random()*el.length)]
+    }
+    bg.style.background=hex;
+}
