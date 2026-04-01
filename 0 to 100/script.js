a000e033aded74ef2dfef8e3022ecec9e75aa6b9
Pashvinder <dhimanpashvinder@gmail.com>
2026-04-01 14:41:18 +0530

    updated

--- 0 to 100/script.js
+++ 0 to 100/script.js
@@ -1,27 +1,33 @@
-function Prime(num){
-    if (num<2) return false;
-    for(let i=2;i<=Math.sqrt(num);i++){
-        if(num%i===0){
+
+function isPrime(num) {
+    if (num < 2) return false;
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
             return false;
         }
     }
+
     return true;
 }
 
-let main=document.getElementById("main")
-for(let i=0;i<=101;i++){
-    let box=document.createElement("div")
-    box.createList.add("box")
-    box.innerText=i;
+let main = document.getElementById("main");
 
-    if(Prime(i)){
-        box.classList.add("prime")
+for (let i = 0; i <= 101; i++) {
+
+    let box = document.createElement("div");
+    box.classList.add("box");
+    box.innerText = i;
+
+    if (isPrime(i)) {
+        box.classList.add("prime");
     }
-    else if(i%2 ==0){
-        box,createList.add("even")
+    else if (i % 2 === 0) {
+        box.classList.add("even");
     }
-    else{
-        box.createList.add("Odd")
+    else {
+        box.classList.add("odd");
     }
-    main.appendChild(box)
+
+    main.appendChild(box);
 }
