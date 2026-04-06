a05deb58421925b202894383009c8f63afb7c695
Pashvinder <dhimanpashvinder@gmail.com>
2026-04-06 10:34:26 +0530

    buttons fixed

--- Todo list/todo.js
+++ Todo list/todo.js
@@ -1,40 +1,58 @@
-let main=document.getElementById("main");
+let main = document.getElementById("main");
 
 function addTask(){
-let taskValue=document.getElementById("task").value;
-let p=document.createElement("p")
-p.style.paddingTop="5px"
+let taskValue = document.getElementById("task").value;
+
+let p = document.createElement("p");
 p.style.margin = "0";
-p.style.padding="10px"
-p.style.borderRadius="5px"
-p.style.fontSize="22px"
-p.style.fontWeight="600"
-p.textContent=taskValue;
+p.style.padding = "10px";
+p.style.borderRadius = "5px";
+p.style.fontSize = "22px";
+p.style.fontWeight = "600";
+p.textContent = taskValue;
 
 
-let compelte=document.createElement("button");
-compelte.textContent="Complete";
-compelte.style.marginLeft="90%"
+let complete = document.createElement("button");
+complete.textContent = "Complete";
+complete.style.backgroundColor = "#11b93b";
+complete.style.border = "none";
+complete.onclick = function () {
+    div.style.backgroundColor = "#28ee00ee"; 
+};
 
+let process = document.createElement("button");
+process.textContent = "In Process"; 
+process.style.backgroundColor = "#c9cc11";
+process.style.border = "none";
+process.onclick = function () {
+    div.style.backgroundColor = "#c9cc11"; 
+};
 
-let progress=document.createElement("button");
-progress.textContent="Complete";
-progress.style.marginLeft="90%"
-
-let cancel=document.createElement("button");
-cancel.textContent="In Progress";
-cancel.style.marginLeft="90%"
-
-
+let cancel = document.createElement("button");
+cancel.textContent = "Cancel"; 
+cancel.style.backgroundColor = "#cc0000";
+cancel.style.border = "none";
+cancel.onclick = function () {
+    div.style.backgroundColor = "#cc1121"; 
+};
 
 let btn = document.createElement("button");
-btn.textContent="DELETE"
+btn.textContent = "DELETE";
 btn.style.backgroundColor = "#ffffff";
 btn.style.color = "black";
 btn.style.border = "none";
 btn.style.padding = "5px 10px";
 btn.style.borderRadius = "5px";
 
+
+let buttondiv = document.createElement("div");
+buttondiv.style.display = "flex";
+buttondiv.style.gap = "10px";
+
+buttondiv.appendChild(process);
+buttondiv.appendChild(complete);
+buttondiv.appendChild(cancel);
+buttondiv.appendChild(btn);
 
 
 let div = document.createElement("div"); 
@@ -47,18 +65,15 @@ div.style.padding = "10px";
 div.style.borderRadius = "20px";
 
 div.appendChild(p); 
-div.appendChild(progress)
-div.appendChild(compelte)
-div.appendChild(cancel)
-div.appendChild(btn); 
+div.appendChild(buttondiv); 
 
 main.appendChild(div);
 
 
-    btn.onclick = function () {
+// delete function
+btn.onclick = function () {
     div.remove();
 };
 
-
-document.getElementById("task").value="";
+document.getElementById("task").value = "";
 }
