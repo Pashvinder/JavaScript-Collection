06e99f9479a2b62bf6aeed35f9ce7f27130af446
Pashvinder <dhimanpashvinder@gmail.com>
2026-04-06 10:44:48 +0530

    button's color updated

--- Todo list/todo.js
+++ Todo list/todo.js
@@ -14,31 +14,41 @@ p.textContent = taskValue;
 
 let complete = document.createElement("button");
 complete.textContent = "Complete";
-complete.style.backgroundColor = "#11b93b";
+complete.style.backgroundColor = "#28ee004d";
 complete.style.border = "none";
+complete.style.padding = "5px 10px";
+complete.style.borderRadius="5px";
+complete.style.color="Green"
 complete.onclick = function () {
-    div.style.backgroundColor = "#28ee00ee"; 
+    div.style.backgroundColor = "#28ee00"; 
 };
 
 let process = document.createElement("button");
 process.textContent = "In Process"; 
-process.style.backgroundColor = "#c9cc11";
+process.style.backgroundColor = "#d3d60044";
+process.style.color = "#dadd36";
 process.style.border = "none";
+process.style.padding = "5px 10px";
+process.style.borderRadius="5px";
 process.onclick = function () {
     div.style.backgroundColor = "#c9cc11"; 
 };
 
 let cancel = document.createElement("button");
 cancel.textContent = "Cancel"; 
-cancel.style.backgroundColor = "#cc0000";
+cancel.style.color = "#cc0000";
+cancel.style.backgroundColor = "#cc000054";
 cancel.style.border = "none";
+cancel.style.padding = "25px 10px";
+cancel.style.borderRadius="90px";
+
 cancel.onclick = function () {
     div.style.backgroundColor = "#cc1121"; 
 };
 
 let btn = document.createElement("button");
 btn.textContent = "DELETE";
-btn.style.backgroundColor = "#ffffff";
+btn.style.backgroundColor = "#0000005e";
 btn.style.color = "black";
 btn.style.border = "none";
 btn.style.padding = "5px 10px";
@@ -59,7 +69,7 @@ let div = document.createElement("div");
 div.style.display = "flex";
 div.style.justifyContent = "space-between";
 div.style.alignItems = "center";
-div.style.backgroundColor = "#aaa"; 
+div.style.backgroundColor = "#aaaaaa52"; 
 div.style.margin = "10px 0";
 div.style.padding = "10px";
 div.style.borderRadius = "20px";
