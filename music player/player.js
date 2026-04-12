c53fb7cc67e489dce6d97a4124fdbfdf420dbdfd
Pashvinder <dhimanpashvinder@gmail.com>
2026-04-12 13:11:12 +0530

    javascripted created

--- music player/player.js
+++ music player/player.js
@@ -1,1 +1,10 @@
-    
+let play=document.getElementById("play")
+let song=document.getElementById("paigaam")
+function TogglePlay(){
+    if(song.paused){
+        song.play();
+    }
+    else{
+        song.paused();
+    }
+}
