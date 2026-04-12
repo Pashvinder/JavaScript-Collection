3d6ca1170e26aa0c5d926bb4153024e7372e03b2
Pashvinder <dhimanpashvinder@gmail.com>
2026-04-12 13:57:58 +0530

    code fixed with random player and pause button

--- music player/player.js
+++ music player/player.js
@@ -1,10 +1,29 @@
-let play=document.getElementById("play")
-let song=document.getElementById("paigaam")
-function TogglePlay(){
-    if(song.paused){
+let song = document.getElementById("audio");
+
+let songs = [
+    "songs/BAADSHAHI.mp3",
+    "songs/DASS KUDIYE.mp3",
+    "songs/Dildarian - Amrinder Gill.mp3",
+    "songs/DOORIYAN.mp3",
+    "songs/DUNIYA.mp3",
+    "songs/PAIGAAM.mp3",
+    "songs/PARDHAANI.mp3",
+    "songs/QUEEN.mp3",
+    "songs/SAMJHA.mp3",
+    "songs/SEARCHING.mp3",
+    "songs/WHY.mp3"
+];
+
+function TogglePlay() {
+    if (song.paused) {
         song.play();
-    }
-    else{
+    } else {
         song.pause();
     }
+}
+
+function PlayRandom() {
+    let randomIndex = Math.floor(Math.random() * songs.length);
+    song.src = songs[randomIndex];
+    song.play();
 }
