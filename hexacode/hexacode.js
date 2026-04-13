945564eb235a4b0903a649f0956c956cff05838a
Pashvinder <dhimanpashvinder@gmail.com>
2026-04-13 10:30:46 +0530

    added body click(not working rn) with addEvenetListener

--- hexacode/hexacode.js
+++ hexacode/hexacode.js
@@ -1,5 +1,5 @@
 let el=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
-
+let body=document.body;
 let bg=document.body;
 let btn=document.getElementById("change")
 btn.onclick=function(){
@@ -7,5 +7,8 @@ btn.onclick=function(){
     for(let i=0;i<6;i++){
     hex+=el[Math.floor(Math.random()*el.length)]
     }
+body.addEventListener("click",bg)
+function bg(){
     bg.style.background=hex;
+}
 }
