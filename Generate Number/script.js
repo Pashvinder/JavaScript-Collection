73750713cb940c94ed2d384b90e4fe4174420695
Pashvinder <dhimanpashvinder@gmail.com>
2026-04-13 09:37:52 +0530

    code completed

---
+++ Generate Number/script.js
@@ -0,0 +1,53 @@
+
+function isPrime(num) {
+    if (num < 2) return false;
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+let main = document.getElementById("main");
+function generateNumber(){
+
+let inputNumber=document.getElementById("inputNumber").value;
+main.innerHTML="";
+for (let i = 0; i <= inputNumber; i++) {
+
+    let box = document.createElement("div");
+    box.classList.add("box");   
+    box.textContent = i;
+    box.style.height = "80px";
+    box.style.width = "80px";
+    main.style.display = "flex";
+    main.style.flexWrap = "wrap";
+    main.style.gap = "5px";  
+    box.style.justifyContent = "center";
+    box.style.alignItems = "center";
+    box.style.fontSize = "20px";
+    box.style.color = "white";
+    box.style.borderRadius = "8px"; 
+    box.style.margin="5px"
+    
+
+    if (isPrime(i)) {
+        box.style.backgroundColor = "red";
+    }
+    else if (i % 2 === 0) {
+        box.style.backgroundColor = "green";
+    }
+    else {
+        box.style.backgroundColor = "yellow";
+        box.style.color = "black";
+    }
+
+    main.appendChild(box);
+}
+}
+
+
+
