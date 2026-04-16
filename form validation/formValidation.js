400bd4d0e12902f20ca1dafa29154a95ca998eae
Pashvinder <dhimanpashvinder@gmail.com>
2026-04-16 14:25:50 +0530

    values extracted from the form

---
+++ form validation/formValidation.js
@@ -0,0 +1,6 @@
+let name=document.getElementById("name").value;
+let email=document.getElementById("email").value;
+let number=document.getElementById("number").value;
+let password=document.getElementById("password").value;
+document.getElementById("myform").addEventListener("submit",function(e));
+
