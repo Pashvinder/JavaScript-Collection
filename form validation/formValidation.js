fd384f27f542c713ec1cf19e3cc0b677df2ae6e8
Pashvinder <dhimanpashvinder@gmail.com>
2026-04-16 14:41:23 +0530

    form validations added

--- form validation/formValidation.js
+++ form validation/formValidation.js
@@ -1,6 +1,30 @@
-let name=document.getElementById("name").value;
-let email=document.getElementById("email").value;
-let number=document.getElementById("number").value;
-let password=document.getElementById("password").value;
-document.getElementById("myform").addEventListener("submit",function(e));
+document.getElementById("myform").addEventListener("submit", function (e) {
 
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("mail").value;
+    let number = document.getElementById("number").value;
+    let password = document.getElementById("pass").value;
+
+    if (name === "") {
+        alert("Name cannot be empty");
+        e.preventDefault();
+    }
+
+    if (!email.includes("@")) {
+        alert("Enter valid Email");
+        e.preventDefault();
+    }
+
+    let contactPattern = /^[0-9]{10}$/;
+    if (!contactPattern.test(number)) {
+        alert("Enter valid 10 digit contact number");
+        e.preventDefault();
+    }
+
+    let passwordPattern = /^(?=.*[A-Z])(?=.*\d).{6,}$/;
+    if (!passwordPattern.test(password)) {
+        alert("Password must be at least 6 characters long and include 1 uppercase letter and 1 number");
+        e.preventDefault();
+    }
+
+});
